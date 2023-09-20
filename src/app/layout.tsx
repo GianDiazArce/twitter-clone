89455f6b8672ca0a781d1089ce055fef500b51e1
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2023-09-20 16:40:19 -0500

    add navbar in layout when user is authenticated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,39 @@
-import './globals.css'
 import '@sweetalert2/theme-dark/dark.css'
 import type { Metadata } from 'next'
+import { NavbarComponent } from './components/navbar'
+import './globals.css'
 import { Providers } from './providers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { type Database } from './types/database'
 
 export const metadata: Metadata = {
   title: 'Clon de twitter',
   description: 'Generando un clon de twitter con nextjs'
 }
 
-export default function RootLayout ({
+export default async function RootLayout ({
   children
 }: {
   children: React.ReactNode
 }) {
+  const supabase = createServerComponentClient<Database>({ cookies })
+  const { data: { session } } = await supabase.auth.getSession()
+  console.log(session)
   return (
     <html lang="en" className='dark'>
       <body>
         <Providers>
+          {
+            session !== null &&
+              (
+                <NavbarComponent
+                  avatarUrl={session?.user?.user_metadata?.avatar_url}
+                  email={session?.user?.email}
+                />
+              )
+
+          }
           {children}
         </Providers>
       </body>
